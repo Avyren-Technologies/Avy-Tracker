19f67439f9cde1f12ff179760ee1676dbe8145dd
CHETAN B R <chiranjeevichetan1996@gmail.com>
2025-09-29 16:21:31 +0530

    Fixed yaml file

--- app.config.ts
+++ app.config.ts
@@ -7,7 +7,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Avy Tracker",
   slug: "avy-tracker",
-  version: "1.1.2",
+  version: "1.1.3",
   orientation: "portrait",
   icon: "./assets/images/adaptive-icon.png",
   scheme: "myapp",
